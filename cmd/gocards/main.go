ad09103147d4a5f826702fe26a929e755316aa90
Michael Goppert <goppert@utexas.edu>
2024-12-15 19:49:21 -0600

    Project structure init

---
+++ cmd/gocards/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/migopp/gocards/internal/debug"
+)
+
+func main() {
+	debug.Printf("| INIT\n")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/migopp/gocards
+
+go 1.23.0
