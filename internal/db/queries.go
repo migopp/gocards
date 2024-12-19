2a4e00d5523465f58f6ab9239c34cf7970d86cb5
Michael Goppert <goppert@utexas.edu>
2024-12-18 23:24:49 -0600

    Formatting

--- internal/db/queries.go
+++ internal/db/queries.go
@@ -41,34 +41,34 @@ func tablesInit() error {
 
 	// `users`
 	query = `CREATE TABLE IF NOT EXISTS users(
-		user_id SERIAL PRIMARY KEY,
-		user_name VARCHAR(50) NOT NULL UNIQUE,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-	);`
+			user_id SERIAL PRIMARY KEY,
+			user_name VARCHAR(50) NOT NULL UNIQUE,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		);`
 	if _, err = db.Exec(query); err != nil {
 		return fmt.Errorf("Error creating `users`: %v", err)
 	}
 
 	// `decks`
 	query = `CREATE TABLE IF NOT EXISTS decks(
-		deck_id SERIAL PRIMARY KEY,
-		user_id INT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
-		deck_name VARCHAR(100) NOT NULL,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		UNIQUE (user_id, deck_name)
-	);`
+			deck_id SERIAL PRIMARY KEY,
+			user_id INT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+			deck_name VARCHAR(100) NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+			UNIQUE (user_id, deck_name)
+		);`
 	if _, err = db.Exec(query); err != nil {
 		return fmt.Errorf("Error creating `decks`: %v", err)
 	}
 
 	// `cards`
 	query = `CREATE TABLE IF NOT EXISTS cards(
-		card_id SERIAL PRIMARY KEY,
-		deck_id INT NOT NULL REFERENCES decks(deck_id) ON DELETE CASCADE,
-		FRONT TEXT NOT NULL,
-		BACK TEXT NOT NULL,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-	);`
+			card_id SERIAL PRIMARY KEY,
+			deck_id INT NOT NULL REFERENCES decks(deck_id) ON DELETE CASCADE,
+			FRONT TEXT NOT NULL,
+			BACK TEXT NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		);`
 	if _, err = db.Exec(query); err != nil {
 		return fmt.Errorf("Error creating `cards`: %v", err)
 	}
